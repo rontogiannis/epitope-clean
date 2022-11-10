efdfbe77ecf4f22dfddfb7e0f9e77dc050b02a3f
rontogiannis <aronto@rosetta8.csail.mit.edu>
2022-11-10 13:58:27 -0500

    added code to calculate confidence

--- data/calculate_structures.py
+++ data/calculate_structures.py
@@ -7,15 +7,18 @@ import re
 
 from tqdm.auto import tqdm
 
-print("Loading ESMFold.")
+make_predictions = False
 
-model = esm.pretrained.esmfold_v1()
-model = model.eval().cuda()
+if make_predictions :
+    print("Loading ESMFold.")
 
-print("Loaded.")
+    model = esm.pretrained.esmfold_v1()
+    model = model.eval().cuda()
+
+    print("Loaded.")
 
 path = "BP3C50ID/test.fasta"
-out = "BP3C50ID/dump/"
+out = "BP3C50ID/test-pred/"
 
 with open(f"{path}", "r") as f :
     lines_with_sn = f.readlines()
@@ -27,11 +30,12 @@ for i in tqdm(range(0, len(lines), 2)) :
     pdb_chain_id = lines[i][1:]
     sequence = lines[i+1].upper()
 
-    with torch.no_grad():
-        output = model.infer_pdb(sequence)
+    if make_predictions :
+        with torch.no_grad():
+            output = model.infer_pdb(sequence)
 
-    with open(f"{out}{pdb_chain_id}.pdb", "w") as f :
-        f.write(output)
+        with open(f"{out}{pdb_chain_id}.pdb", "w") as f :
+            f.write(output)
 
     struct = bsio.load_structure(f"{out}{pdb_chain_id}.pdb", extra_fields=["b_factor"])
     pLDDTs.append(struct.b_factor.mean())
@@ -39,16 +43,14 @@ for i in tqdm(range(0, len(lines), 2)) :
 mu = np.mean(pLDDTs)
 sigma = np.std(pLDDTs)
 
-n, bins, patches = plt.hist(pLDDTs, 5, density=True, facecolor='g', alpha=0.75)
+n, bins, patches = plt.hist(pLDDTs, 20, facecolor='g', alpha=0.75)
 
 plt.xlabel('pLDDT Score')
 plt.ylabel('Number of Observations')
 plt.title('Certainty of Predicted Structures')
-plt.text(60, .025, fr'$\mu=${mu}, $\sigma=${sigma}')
+plt.text(50, len(pLDDTs)/2, fr'$\mu=${mu:.2f}, $\sigma=${sigma:.2f}')
 plt.xlim(0, 100)
 plt.ylim(0, len(pLDDTs))
 plt.grid(True)
 plt.savefig("hist.png")
-
-
 
